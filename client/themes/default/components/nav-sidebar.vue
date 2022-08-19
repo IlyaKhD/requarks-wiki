2669be5091d16c5724aa23e6b09a21ff0e8e417a
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-08-19 21:13:02 +0400

    Make Custom navigation less confusing

--- client/themes/default/components/nav-sidebar.vue
+++ client/themes/default/components/nav-sidebar.vue
@@ -1,14 +1,6 @@
 <template lang="pug">
   div
     .pa-3.d-flex(v-if='navMode === `MIXED`', :class='$vuetify.theme.dark ? `grey darken-5` : `blue darken-3`')
-      v-btn(
-        depressed
-        :color='$vuetify.theme.dark ? `grey darken-4` : `blue darken-2`'
-        style='min-width:0;'
-        @click='goHome'
-        :aria-label='$t(`common:header.home`)'
-        )
-        v-icon(size='20') mdi-home
       v-btn.ml-3(
         v-if='currentMode === `custom`'
         depressed
@@ -25,8 +17,8 @@
         style='flex: 1 1 100%;'
         @click='switchMode(`custom`)'
         )
-        v-icon(left) mdi-navigation
-        .body-2.text-none {{$t('common:sidebar.mainMenu')}}
+        v-icon(left) mdi-pin
+        .body-2.text-none Pinned Pages
     v-divider
     //-> Custom Navigation
     v-list.py-2(v-if='currentMode === `custom`', dense, :class='color', :dark='dark')
@@ -236,7 +228,7 @@ export default {
     } else if (this.navMode === 'STATIC') {
       this.currentMode = 'custom'
     } else {
-      this.currentMode = window.localStorage.getItem('navPref') || 'custom'
+      this.currentMode = 'browse'
     }
     if (this.currentMode === 'browse') {
       this.loadFromCurrentPath()
